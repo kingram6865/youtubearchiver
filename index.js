9b3ca2ca9afc0ca027bf1c6da8f61468e7ce78e4
Ken Ingram <ken.ingram@gmail.com>
2025-04-28 18:34:45 -0700

    Refactoring

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ import { getVideoData } from './lib/videoInfo.js'
 
 async function execute(input) {
   let videoResult = await getVideoData(input)
-  // let channelResult = await getChannelData(input);
-  console.log(videoResult);
+  let channel = videoResult.videoData.api.channelId
+
+  let channelResult = await getChannelData(channel);
+
+  // console.log(JSON.stringify(videoResult, null, 2));
+  console.log(JSON.stringify(channelResult, null, 2));
+
 }
 
 execute(process.argv[2]);
